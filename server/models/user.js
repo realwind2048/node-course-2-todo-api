74140dfe2a8c699ae594ee035a27168ccd8e2e79
realwind2048 <realwind2048@gmail.com>
2018-08-20 14:54:21 +0900

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -1,55 +1,26 @@
-const mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  // console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// save new something
-var newTodo = new Todo({
-  text: 'Edit this video'
-  // completed: false,
-  // completedAt: 25
-});
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-//
-// User
-// email - require, trim, type, min length 1
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-var newUser = new User({
-  email: 'realwind2048@gmail.com'
-});
-
-newUser.save().then((doc) => {
-  console.log('Save user', JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unabe to save user', e);
+app.listen(3000, () => {
+  console.log('Started on part 3000');
 });
