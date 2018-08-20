9dacdc86ffd4119b7b8071ab3cdc34dcda4865be
realwind2048 <realwind2048@gmail.com>
2018-08-20 13:57:12 +0900

    Add mongoose save

--- server/server.js
+++ server/server.js
@@ -5,25 +5,51 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 // save new something
 var newTodo = new Todo({
-  text: 'Cook breakfast',
-  completed: false,
-  completedAt: 25
+  text: 'Edit this video'
+  // completed: false,
+  // completedAt: 25
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', JSON.stringify(doc, undefined, 2));
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
+//
+// User
+// email - require, trim, type, min length 1
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var newUser = new User({
+  email: 'realwind2048@gmail.com'
+});
+
+newUser.save().then((doc) => {
+  console.log('Save user', JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unabe to save user', e);
 });
