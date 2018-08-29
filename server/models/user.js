3464fc040eac8241c79df85814251f30a8cc9abb
realwind2048 <realwind2048@gmail.com>
2018-08-29 21:46:08 +0900

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -44,7 +44,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'secret123');
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat([{access, token}]);
   return user.save().then(() => {
@@ -66,7 +66,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'secret123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
